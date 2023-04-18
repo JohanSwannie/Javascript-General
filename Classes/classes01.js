42b0ede041f25685c0c2db9af429fc49569bec99
jswan <jswannieman@gmail.com>
2023-04-18 15:55:12 +1000

    Update classes01 Javascript Component

--- Classes/classes01.js
+++ Classes/classes01.js
@@ -7,19 +7,19 @@ class Rectangle {
   getRectangleArea() {
     return `The area of the ${this.color} rectangle is ${
       this.width * this.height
-    } meters`;
+    } square meters`;
   }
 
   set anotherObject(value) {
     this.width = value;
-    this.height = value;
+    this.height = 20;
     this.color = "orange";
   }
 
   get theAreas() {
     return `The area of the ${this.color} rectangle is now ${
       this.width * this.height
-    } meters`;
+    } square meters`;
   }
 }
 
