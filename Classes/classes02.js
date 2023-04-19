c4269a44e3a46de0f02ddd2979ad95e106854bc8
jswan <jswannieman@gmail.com>
2023-04-20 09:58:07 +1000

    Update classes02 Javascript Component

--- Classes/classes02.js
+++ Classes/classes02.js
@@ -16,12 +16,9 @@ class Box {
 let box1 = new Box(12);
 for (var value in box1) {
   if (box1.hasOwnProperty(value)) {
-    document.write(`<span style='font-family: "Trirong", serif; color:
-    #FFF'>${value} : ${box1[value]} </span> </br>`);
+    document.write(`<span>${value} : ${box1[value]}</spanl></br>`);
   }
 }
 
-document.write(`<span style='font-family: "Trirong", serif; color:
-#FFF'>${Box.test1(14, 14)} </span>  </br>`);
-document.write(`<span style='font-family: "Trirong", serif; color:
-#FFF'>${Box.test2(13, 13)} </span>`);
+document.write(`<span>${Box.test1(14, 14)}</span></br>`);
+document.write(`<span>${Box.test2(13, 13)}</span>`);
