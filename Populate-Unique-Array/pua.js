02932b2429a000676162cdc2e6402609de8b07ed
jswan <jswannieman@gmail.com>
2023-04-22 11:49:32 +1000

    Update pua Javascript Component

--- Populate-Unique-Array/pua.js
+++ Populate-Unique-Array/pua.js
@@ -1,26 +1,42 @@
-let detail = document.getElementById("detail");
+let detail1 = document.getElementById("detail1");
+let detail2 = document.getElementById("detail2");
 let inner = document.getElementById("inner");
 let error = document.getElementById("error");
 
-let array = [12, 8, 5, 18, 10, 22, 39, 4, 17, 30];
+let array1 = [12, 8, 5, 18, 10, 22, 39, 4, 17, 30];
+let array2 = [];
 
-detail.innerHTML = array;
+detail1.innerHTML = array1;
+
+function errorInput(inputValue) {
+  inner.value = "";
+  error.innerHTML = `${inputValue} already exists`;
+  error.style.display = "block";
+  setTimeout(() => {
+    error.innerHTML = "";
+    error.style.display = "none";
+  }, 2000);
+}
 
 function takeIt() {
   let inputValue = parseInt(inner.value);
-  let IDX = array.indexOf(inputValue);
+  let IDX = array1.indexOf(inputValue);
   if (IDX === -1) {
-    array.push(inputValue);
-    detail.innerHTML = array;
+    if (detail1.offsetWidth > 350) {
+      let IDX2 = array2.indexOf(inputValue);
+      if (IDX2 === -1) {
+        array2.push(inputValue);
+        detail2.innerHTML = array2;
+        detail2.style.display = "block";
+      } else {
+        errorInput(inputValue);
+      }
+    } else {
+      array1.push(inputValue);
+      detail1.innerHTML = array1;
+    }
     inner.value = "";
-    error.style.display = "none";
   } else {
-    inner.value = "";
-    error.innerHTML = `${inputValue} is already in the array`;
-    error.style.display = "block";
-    setTimeout(() => {
-      error.innerHTML = "";
-      error.style.display = "none";
-    }, 2000);
+    errorInput(inputValue);
   }
 }
