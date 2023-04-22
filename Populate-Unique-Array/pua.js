3c33054246b2e88f918269b759207885e9b59389
jswan <jswannieman@gmail.com>
2023-04-22 13:46:09 +1000

    Update pua Javascript Component

--- Populate-Unique-Array/pua.js
+++ Populate-Unique-Array/pua.js
@@ -5,7 +5,7 @@ let inner = document.getElementById("inner");
 let error = document.getElementById("error");
 let error2 = document.getElementById("error2");
 
-let array1 = [12, 8, 5, 18, 10, 22, 39, 4, 17, 30];
+let array1 = [1, 2, 3, 4, 5];
 let array2 = [];
 let array3 = [];
 
@@ -27,6 +27,7 @@ function takeIt() {
     if (inputValue > 100 || inputValue < -100) {
       error2.style.display = "block";
       error2.innerHTML = `Maximum value = 100 & Minimum value = -100`;
+      inner.value = "";
       setTimeout(() => {
         error2.innerHTML = "";
         error2.style.display = "none";
@@ -34,17 +35,24 @@ function takeIt() {
     } else {
       let IDX = array1.indexOf(inputValue);
       if (IDX === -1) {
-        if (array1.length === 20) {
+        if (array1.length === 7) {
           let IDX2 = array2.indexOf(inputValue);
           if (IDX2 === -1) {
-            if (array2.length === 20) {
+            if (array2.length === 7) {
               let IDX3 = array3.indexOf(inputValue);
-              if (IDX3 === -1) {
-                array3.push(inputValue);
-                detail3.innerHTML = array3;
-                detail3.style.display = "block";
+              if (array3.length === 7) {
+                error3.style.display = "block";
+                error3.innerHTML = `All Arrays now completed`;
+                inner.value = "";
+                inner.hidden = true;
               } else {
-                errorInput(inputValue);
+                if (IDX3 === -1) {
+                  array3.push(inputValue);
+                  detail3.innerHTML = array3;
+                  detail3.style.display = "block";
+                } else {
+                  errorInput(inputValue);
+                }
               }
             } else {
               array2.push(inputValue);
