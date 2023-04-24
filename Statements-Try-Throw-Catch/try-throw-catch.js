739142a16c0de405139855f6203bd5393f918803
jswan <jswannieman@gmail.com>
2023-04-24 15:28:58 +1000

    Update try-throw-catch Javascript Component

--- Statements-Try-Throw-Catch/try-throw-catch.js
+++ Statements-Try-Throw-Catch/try-throw-catch.js
@@ -1,3 +1,14 @@
+let array = [];
+
+const displayArray = document.getElementById("array");
+
+let count = 1;
+
+const addToArray = (inputValue) => {
+  array.push(inputValue);
+  displayArray.innerHTML = array;
+};
+
 const checkInput = () => {
   const errorMsg = document.getElementById("errorMsg");
   const input = document.getElementById("input");
@@ -12,6 +23,14 @@ const checkInput = () => {
       throw `Number is too small`;
     } else if (inputValue > 30) {
       throw `Number is too big`;
+    } else if (count < 11) {
+      displayArray.style.display = "block";
+      addToArray(inputValue);
+      count++;
+      throw `Number Accepted - Thanks`;
+    } else {
+      input.hidden = true;
+      throw `Input now CLOSED`;
     }
   } catch (error) {
     errorMsg.innerHTML = `${error}`;
