5116f210a18d01eb52594b9da5e1f12544b72ecf
jswan <jswannieman@gmail.com>
2023-04-27 10:17:47 +1000

    Commit bitwise Javascript Component

---
+++ Operators/Bitwise-Operators/bitwise.js
@@ -0,0 +1,13 @@
+let val1 = 15,
+  val2 = 4;
+
+document.getElementById("bitwise1").innerHTML = `The result is ${val1 & val2}`;
+
+let val3 = 10,
+  val4 = 7;
+
+document.getElementById("bitwise2").innerHTML = `The result is ${val3 | val4}`;
+
+let val5 = 9;
+
+document.getElementById("bitwise3").innerHTML = `The result is ${~val5}`;
