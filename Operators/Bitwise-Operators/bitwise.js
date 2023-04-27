bfd8d3106ae6fff9d8abeca7b2987c2165734f35
jswan <jswannieman@gmail.com>
2023-04-27 14:08:33 +1000

    Update bitwise Javascript Component

--- Operators/Bitwise-Operators/bitwise.js
+++ Operators/Bitwise-Operators/bitwise.js
@@ -11,3 +11,20 @@ document.getElementById("bitwise2").innerHTML = `The result is ${val3 | val4}`;
 let val5 = 9;
 
 document.getElementById("bitwise3").innerHTML = `The result is ${~val5}`;
+
+let val6 = 5,
+  val7 = 3;
+
+document.getElementById("bitwise4").innerHTML = `The result is ${val6 ^ val7}`;
+
+let val8 = 5,
+  val9 = 2;
+
+document.getElementById("bitwise5").innerHTML = `The result is ${val8 >> val9}`;
+
+let val10 = 4,
+  val11 = 3;
+
+document.getElementById("bitwise6").innerHTML = `The result is ${
+  val10 << val11
+}`;
