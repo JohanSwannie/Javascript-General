a421c10edebcbe45f1def29d43caa2e348dc14c5
jswan <jswannieman@gmail.com>
2023-04-27 14:22:07 +1000

    Commit typeof Javascript Component

---
+++ Operators/Typeof-Operator/typeof.js
@@ -0,0 +1,42 @@
+let val1 = "The ducks swim in the lake",
+  val2 = 23,
+  val3 = true,
+  val4 = undefined,
+  val5 = { name: "Mary" },
+  val6 = [1, 3, 44];
+
+let val7 = () => {};
+
+let val8 = null;
+
+document.getElementById(
+  "typeof1"
+).innerHTML = `the type of "val1" is - ${typeof val1}`;
+
+document.getElementById(
+  "typeof2"
+).innerHTML = `the type of "val2" is - ${typeof val2}`;
+
+document.getElementById(
+  "typeof3"
+).innerHTML = `the type of "val3" is - ${typeof val3}`;
+
+document.getElementById(
+  "typeof4"
+).innerHTML = `the type of "val4" is - ${typeof val4}`;
+
+document.getElementById(
+  "typeof5"
+).innerHTML = `the type of "val5" is - ${typeof val5}`;
+
+document.getElementById(
+  "typeof6"
+).innerHTML = `the type of "val6" is - ${typeof val6}`;
+
+document.getElementById(
+  "typeof7"
+).innerHTML = `the type of "val7" is - ${typeof val7}`;
+
+document.getElementById(
+  "typeof8"
+).innerHTML = `the type of "val8" is - ${typeof val8}`;
